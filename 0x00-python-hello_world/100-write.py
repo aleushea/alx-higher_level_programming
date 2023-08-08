0411b73fb8aa7577802159ecf7b0c2d9a698bfcb
aleushea <aleushea@gmail.com>
2023-08-08 01:35:54 -0700

    revised

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,2 +1,5 @@
-#!/bin/bash
-python -m compileall -b $PYFILE i
+#!/usr/bin/python3
+import sys
+str = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+sys.stderr.write(str)
+sys.exit(1)

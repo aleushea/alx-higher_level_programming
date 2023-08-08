519483b3eebaa26c4c7a978d2529cc0962f803a2
aleushea <aleushea@gmail.com>
2023-08-08 01:16:38 -0700

    revised

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-print("""\tThe Zen of Python, by Tim Peters
+print("""\\tThe Zen of Python, by Tim Peters
 
         Beautiful is better than ugly.
         Explicit is better than implicit.

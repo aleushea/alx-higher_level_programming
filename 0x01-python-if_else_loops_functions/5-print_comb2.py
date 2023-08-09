5df79a22f57cba6e55c8ba7dada3d22197bd680b
aleushea <aleushea@gmail.com>
2023-08-08 20:50:36 -0700

    revised

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    print("{0:02d}".format(i), end=", ")
-    print("{:d}".format(99))
+for i in range(0, 10):
+    for j in range(0, 10):
+        print("{:d}{:d}".format(i, j), end=", ")

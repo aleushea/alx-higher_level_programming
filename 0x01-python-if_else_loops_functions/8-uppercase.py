6312cb5a94ea89042e1ce302bb9a97151d2fc413
aleushea <aleushea@gmail.com>
2023-08-10 14:31:50 -0700

    revised

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,8 @@
-#!/usr/bin/env python3
-uppercase = __import__('8-uppercase').uppercase
-
-uppercase("holberton")
-uppercase("Holberton Scholl 98 Battery street")
+#!/usr/bin/python3
+def uppercase(str):
+    """this script print words in uppercase."""
+    for c in str:
+        if 97 <= ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")

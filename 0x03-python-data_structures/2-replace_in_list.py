7c256a429d39752e91e91eb9c11bd0a96eea9e1d
aleushea <aleushea@gmail.com>
2023-08-13 12:26:32 -0700

    first trial

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
+# question number one-replace_in_list.py
 
-if __name__ == "__main__":
-    import sys
-    arg = sys.argv
-    size = len(arg) - 1
-    if size > 1:
-        print("{} arguments:".format(size))
-        for i in range(1, size + 1):
-            print("{}: {}".format(i, arg[i]))
-    elif size == 0:
-        print("{} arguments.".format(size))
-    else:
-        print("{} argument:".format(size))
-        print("{}: {}".format(size, arg[1]))
+def replace_in_list(my_list, idx, element):
+    if 0 <= idx < len(my_list):
+        my_list[idx] = element
+    return my_list

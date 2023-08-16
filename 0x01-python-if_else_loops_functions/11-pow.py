d8a14f9b28381c2b102f25e826c51f202e814106
aleushea <aleushea@gmail.com>
2023-08-15 20:24:10 -0700

    revised

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python3
-pow = __import__('11-pow').pow
-
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
+#!/usr/bin/python3
+# thsi function is a to the power of b 11-pow.py
+def pow(a, b):
+    return a ** b

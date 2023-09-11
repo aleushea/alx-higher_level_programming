789f4e7c83ca63babf4c45604156e683be58b773
aleushea <aleushea@gmail.com>
2023-09-11 12:26:22 -0700

    0x0A. Python - Inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,1 +1,13 @@
-#!
+#!/usr/bin/python3
+"""
+The lookup function container
+"""
+
+
+def lookup(obj):
+    """
+    Args:the function named lookup(obj) takes a single parameter obj
+    obj: initial object
+    Returns: return a list of attributes and methods associated with the object
+    """
+    return dir(obj)

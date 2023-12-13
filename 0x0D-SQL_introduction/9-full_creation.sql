df25c93371ffa80341e9e1fbd4b9a59ce88e5df3
aleushea <aleushea@gmail.com>
2023-12-13 05:47:33 -0800

    0x0D. SQL - Introduction

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
 -- this script creates a table second_table in the database hbtn_0c_0 in a MySQL server and add multiples rows.
-CREATE TABLE IF NOT EXISTS second_table (id int, namevarchar(256), score int);
+CREATE TABLE IF NOT EXISTS second_table (id int, name varchar(256), score int);
 INSERT INTO second_table (id, name, score)
 VALUES (1,'John', 10), (2, 'Alex', 3), (3, 'Bob, 14), (4, 'Georgge', 8);

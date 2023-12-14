43c28a4f2941640f3eb0f8df8d753056e2e108f7
aleushea <aleushea@gmail.com>
2023-12-13 18:35:26 -0800

    0x0E. SQL - More queries

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,4 @@
+-- This script lists all shows, and all genres 
+
+-- Apply join to dsplay data and order based on title and name
+SELECT tv_shows.title, tv_genres.name FROM tv_shows LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id ORDER BY tv_shows.title, tv_genres.name;

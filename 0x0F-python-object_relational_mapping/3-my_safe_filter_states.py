8e7b8753cd4eace63896e99dc43257f821ef41b5
aleushea <aleushea@gmail.com>
2024-01-14 17:57:59 -0800

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python3
 """
-this script lists all states from the database hbtn with a given name and is safe from MySQL injections
+This script lists all states from the database hbtn with a given name and is safe from MySQL injections.
 """
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
+    # Retrieve command-line arguments
+    The_username = argv[1]
+    The_password = argv[2]
+    database_name = argv[3]
+    state_name = argv[4]
+
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=The_username,
+                         passwd=The_password, db=database_name, charset="utf8")
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
-                   (argv[4],))
+
+    # Construct the SQL query using a placeholder
+    sql_query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC"
+
+    # Execute the SQL query with the state name as a parameter
+    cursor.execute(sql_query, (state_name,))
+
+    # Fetch all the matching rows
     rows = cursor.fetchall()
+
+    # Display the results
     for row in rows:
         print(row)
+
+    # Close the cursor and database connection
     cursor.close()
     db.close()
